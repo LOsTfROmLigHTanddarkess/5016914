d8ca2cd4c11b3d38e47e41660638d76f5c84744d
LOsTfROmLigHTanddarkess <145378092+LOsTfROmLigHTanddarkess@users.noreply.github.com>
2024-08-07 12:19:25 +0530

    Add files via upload

---
+++ Week 2/Week 2_Spring_core_Maven/Exercise_8/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    // A sample method to demonstrate functionality
+    public void performRepositoryTask() {
+        System.out.println("BookRepository is performing a repository task.");
+    }
+}
