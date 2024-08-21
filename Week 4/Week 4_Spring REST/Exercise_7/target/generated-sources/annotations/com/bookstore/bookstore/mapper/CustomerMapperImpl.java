de75d51589daf77616d41b73d1514bb00a865185
LOsTfROmLigHTanddarkess <145378092+LOsTfROmLigHTanddarkess@users.noreply.github.com>
2024-08-21 15:14:51 +0530

    Add files via upload

---
+++ Week 4/Week 4_Spring REST/Exercise_7/target/generated-sources/annotations/com/bookstore/bookstore/mapper/CustomerMapperImpl.java
@@ -0,0 +1,35 @@
+package com.bookstore.bookstore.mapper;
+
+import com.bookstore.bookstore.dto.CustomerDTO;
+import com.bookstore.bookstore.entity.Customer;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-21T14:06:34+0530",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 21.0.4 (Eclipse Adoptium)"
+)
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Override
+    public CustomerDTO customerToCustomerDTO(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDTO customerDTO = new CustomerDTO();
+
+        return customerDTO;
+    }
+
+    @Override
+    public Customer customerDTOToCustomer(CustomerDTO customerDTO) {
+        if ( customerDTO == null ) {
+            return null;
+        }
+
+        Customer customer = new Customer();
+
+        return customer;
+    }
+}
