d438ce630c3bfe387856c977e921ae437678e68d
LOsTfROmLigHTanddarkess <145378092+LOsTfROmLigHTanddarkess@users.noreply.github.com>
2024-08-21 15:11:42 +0530

    Add files via upload

---
+++ Week 4/Week 4_Spring REST/Exercise_4/src/main/java/com/bookstore/bookstore/controller/CustomerController.java
@@ -0,0 +1,55 @@
+package com.bookstore.bookstore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookstore.bookstore.entity.Customer;
+import com.bookstore.bookstore.repository.CustomerRepository;
+
+
+// Other imports remain the same
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    //Get all customers
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    // POST to create a new customer using JSON request body
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerRepository.save(customer);
+        return ResponseEntity.ok(savedCustomer);
+    }
+    
+    // POST to create a new customer using form data
+    @PostMapping("/form")
+    public ResponseEntity<Customer> createCustomerFormData(
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("address") String address) {
+
+        Customer customer = new Customer();
+        customer.setName(name);
+        customer.setEmail(email);
+        customer.setAddress(address);
+
+        Customer savedCustomer = customerRepository.save(customer);
+        return ResponseEntity.ok(savedCustomer);
+    }
+}
